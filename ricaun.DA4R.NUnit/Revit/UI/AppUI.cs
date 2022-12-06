6dbb0808d3db50b9d46c0cca720a13d8e66f02ee
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2022-12-06 11:40:24 -0300

    Update ricaun.NUnit to 1.0.9

--- ricaun.DA4R.NUnit/Revit/UI/AppUI.cs
+++ ricaun.DA4R.NUnit/Revit/UI/AppUI.cs
@@ -2,6 +2,7 @@ using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using ricaun.Revit.UI;
 using System;
+using System.Linq;
 
 namespace ricaun.DA4R.NUnit.Revit.UI
 {
@@ -14,6 +15,26 @@ namespace ricaun.DA4R.NUnit.Revit.UI
             ribbonPanel = application.CreatePanel("DA4R");
             ribbonPanel.CreatePushButton<Commands.Command>()
                 .SetLargeImage(Properties.Resources.Revit.GetBitmapSource());
+
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine(this.GetType().Assembly.FullName);
+            Console.WriteLine(typeof(ricaun.NUnit.TestEngine).Assembly.FullName);
+            Console.WriteLine($"ricaun.NUnit {ricaun.NUnit.TestEngine.Version} {ricaun.NUnit.TestEngine.VersionNUnit}");
+            Console.WriteLine("--------------------------------------------------");
+
+            foreach (var item in AppDomain.CurrentDomain.GetAssemblies().Where(e => e.GetName().Name == "nunit.framework"))
+            {
+                Console.WriteLine($"{item} \t{item.Location}");
+            }
+            Console.WriteLine("--------------------------------------------------");
+            //foreach (var item in AppDomain.CurrentDomain.GetAssemblies().Where(e => e.GetName().Name.Contains("Dynamo")))
+            //{
+            //    Console.WriteLine($"{item} {item.Location}");
+            //}
+            //Console.WriteLine("--------------------------------------------------");
+            //Console.WriteLine(typeof(NUnitAttribute).Assembly);
+
+
             return Result.Succeeded;
         }
 
