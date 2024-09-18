342cb73571424731a265d5ccb8540425dbe21d47
Luiz Henrique Cassettari <ricaun@gmail.com>
2024-09-18 12:48:18 -0300

    Prerelease rc

--- ricaun.DA4R.NUnit.Tests/Tests.cs
+++ ricaun.DA4R.NUnit.Tests/Tests.cs
@@ -3,16 +3,6 @@ using Autodesk.Revit.DB;
 using NUnit.Framework;
 using System;
 
-[assembly: System.Reflection.AssemblyMetadata("NUnit.Timeout", "5")] // Set timeout 5 minutes
-[assembly: System.Reflection.AssemblyMetadata("NUnit.Verbosity", "3")]
-#if !DEBUG
-[assembly: System.Reflection.AssemblyMetadata("NUnit.Application", "RICAUN_REVIT_TEST_APPLICATION_DA4R_ONLINE_TEST")]
-#endif
-
-#if DEBUG
-[assembly: System.Reflection.AssemblyMetadata("NUnit.Application", "..\\..\\..\\..\\ricaun.DA4R.NUnit\\bin\\ReleaseFiles\\ricaun.DA4R.NUnit.Console.zip")]
-#endif
-
 namespace ricaun.DA4R.NUnit.Tests
 {
     public class Tests
