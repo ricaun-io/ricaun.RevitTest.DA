3eb68a658dfdd41ef4508c7fb7a93f709531591d
Luiz Henrique Cassettari <ricaun@gmail.com>
2025-02-19 18:26:00 -0300

    Update `RevitApplication`

--- ricaun.DA4R.NUnit/RevitExtensions/RevitApplication.cs
+++ ricaun.DA4R.NUnit/RevitExtensions/RevitApplication.cs
@@ -7,27 +7,42 @@
     using System.Reflection;
 
     /// <summary>
-    /// RevitApplication
+    /// Provides utility methods and properties for interacting with the Revit application.
     /// </summary>
+    /// <remarks>Source: <a href="https://github.com/ricaun-io/ricaun.Revit.UI/blob/master/ricaun.Revit.UI/RevitApplication.cs"/> </remarks>
     public static class RevitApplication
     {
         /// <summary>
-        /// UIApplication
+        /// Gets the current <see cref="UIApplication"/> instance.
         /// </summary>
-        public static UIApplication UIApplication { get; } = new RibbonItemEventArgs().Application;
+        public static UIApplication UIApplication => new RibbonItemEventArgs().Application;
         /// <summary>
-        /// UIControlledApplication
+        /// Gets the current <see cref="UIControlledApplication"/> instance.
         /// </summary>
-        public static UIControlledApplication UIControlledApplication { get; } = GetUIControlledApplication();
+        public static UIControlledApplication UIControlledApplication => GetUIControlledApplication(UIApplication);
+        /// <summary>
+        /// Gets a value indicating whether the current context is within an add-in.
+        /// </summary>
+        public static bool IsInAddInContext => InAddInContext(UIApplication);
+
         #region Private
-        private static UIControlledApplication GetUIControlledApplication()
+        /// <summary>
+        /// Get <see cref="Autodesk.Revit.UI.UIControlledApplication"/> using the <paramref name="application"/>
+        /// </summary>
+        /// <param name="application">Revit UIControlledApplication</param>
+        private static UIControlledApplication GetUIControlledApplication(UIApplication application)
         {
             var type = typeof(UIControlledApplication);
 
             var constructor = type.GetConstructor(
-                BindingFlags.Instance | BindingFlags.NonPublic, null, new Type[] { UIApplication.GetType() }, null);
+                BindingFlags.Instance | BindingFlags.NonPublic, null, new Type[] { application.GetType() }, null);
 
-            return constructor?.Invoke(new object[] { UIApplication }) as UIControlledApplication;
+            return constructor?.Invoke(new object[] { application }) as UIControlledApplication;
+        }
+        private static bool InAddInContext(UIApplication uiapp)
+        {
+            // ActiveAddInId is only available when Revit is within an API context.
+            return uiapp.ActiveAddInId is not null;
         }
         #endregion
     }
@@ -40,11 +55,11 @@
         /// <summary>
         /// Application
         /// </summary>
-        public static Application Application { get; } = GetApplication();
+        public static Application Application => GetApplication();
         /// <summary>
         /// ControlledApplication
         /// </summary>
-        public static ControlledApplication ControlledApplication { get; } = GetControlledApplication();
+        public static ControlledApplication ControlledApplication => GetControlledApplication();
 
         #region Private
         private static Application GetApplication()
